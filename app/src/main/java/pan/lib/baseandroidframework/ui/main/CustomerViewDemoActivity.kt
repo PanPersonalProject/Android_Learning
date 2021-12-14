f4a0e43b99c5d45b5eaa288ce51d34de9dfea11c
panqi <12345678>
2021-12-14 17:28:02 +0800

    CameraView Study

--- app/src/main/java/pan/lib/baseandroidframework/ui/main/CustomerViewDemoActivity.kt
+++ app/src/main/java/pan/lib/baseandroidframework/ui/main/CustomerViewDemoActivity.kt
@@ -1,42 +1,33 @@
 package pan.lib.baseandroidframework.ui.main
 
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_customer_view_demo.*
 import pan.lib.baseandroidframework.R
 import pan.lib.common_lib.base.BaseActivity
-import android.animation.AnimatorSet
 
 class CustomerViewDemoActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                cameraView.rotateX3d(progress)
-            }
+        picAnimator()
+    }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-
-        })
-        val bottomFlipAnimator=ObjectAnimator.ofFloat(cameraView,"bottomFlip",45f)
+    private fun picAnimator() {
+        val bottomFlipAnimator = ObjectAnimator.ofFloat(cameraView, "bottomFlip", 45f)
         bottomFlipAnimator.duration = 1500
-        val flipRotationAnimator=ObjectAnimator.ofFloat(cameraView,"flipRotation",270f)
-        bottomFlipAnimator.duration = 1500
-        val topFlipAnimator=ObjectAnimator.ofFloat(cameraView,"topFlip",45f)
+
+        val flipRotationAnimator = ObjectAnimator.ofFloat(cameraView, "flipRotation", 270f)
+        flipRotationAnimator.duration = 1500
+
+        val topFlipAnimator = ObjectAnimator.ofFloat(cameraView, "bottomFlip", 45f, 0f)
         topFlipAnimator.duration = 1500
-
         val animatorSet = AnimatorSet()
-        animatorSet.playSequentially(bottomFlipAnimator)
-//        animatorSet.playSequentially(bottomFlipAnimator, flipRotationAnimator, topFlipAnimator)
+        animatorSet.playSequentially(bottomFlipAnimator, flipRotationAnimator, topFlipAnimator)
         animatorSet.startDelay = 1000
         animatorSet.start()
     }
 
-    override fun getLayoutId()=R.layout.activity_customer_view_demo
+    override fun getLayoutId() = R.layout.activity_customer_view_demo
 }
--- app/src/main/java/pan/lib/baseandroidframework/ui/views/CameraView.kt
+++ app/src/main/java/pan/lib/baseandroidframework/ui/views/CameraView.kt
@@ -1,11 +1,14 @@
 package pan.lib.baseandroidframework.ui.views
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Camera
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.graphics.drawable.toBitmap
+import pan.lib.common_lib.utils.WindowUtil
+import pan.lib.common_lib.utils.printSimpleLog
 
 /**
  * AUTHOR Pan Created on 2021/12/8
@@ -13,14 +16,14 @@ import androidx.core.graphics.drawable.toBitmap
 class CameraView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val camera = Camera()
-    private var flipRotation = 10f
+    private var flipRotation = 0f
         set(value) {
             field = value
             invalidate()
         }
     private var bottomFlip = 0f
         set(value) {
-            Log.e("TAG_GENERAL", "angle=$value")
+            printSimpleLog("bottomFlip=$bottomFlip")
             field = value
             invalidate()
         }
@@ -33,29 +36,15 @@ class CameraView(context: Context, attrs: AttributeSet?) : AppCompatImageView(co
 
 
     init {
-        camera.rotateX(0f)
-        camera.setLocation(0f, 0f, -6 * resources.displayMetrics.density) //更具屏幕分辨率设定z轴英尺
+        camera.setLocation(0f, 0f, WindowUtil.getZForCamera()) //更具屏幕分辨率设定z轴英尺
         paint.style = Paint.Style.FILL
     }
 
     override fun onDraw(canvas: Canvas) {
-        val picWidthHalf = (width / 2).toFloat()
+        if (drawable == null) return
+        val imageWidth = (width.toFloat() / 1.4).toFloat()
+        val picWidthHalf = (imageWidth / 2)
 
-
-       /*   绘制下半部分
-        canvas.save();
-        canvas.translate(PADDING + IMAGE_WIDTH / 2, PADDING + IMAGE_WIDTH / 2);
-        canvas.rotate(-flipRotation);
-        camera.save();
-        camera.rotateX(bottomFlip);
-        camera.applyToCanvas(canvas);
-        camera.restore();
-        canvas.clipRect(- IMAGE_WIDTH, 0, IMAGE_WIDTH, IMAGE_WIDTH);
-        canvas.rotate(flipRotation);
-        canvas.translate(- (PADDING + IMAGE_WIDTH / 2), - (PADDING + IMAGE_WIDTH / 2));
-        canvas.drawBitmap(Utils.getAvatar(getResources(), (int) IMAGE_WIDTH), PADDING, PADDING, paint);
-        canvas.restore();
-        * */
         //图片下半部分
         canvas.save()
         canvas.translate(picWidthHalf, picWidthHalf)
@@ -65,79 +54,35 @@ class CameraView(context: Context, attrs: AttributeSet?) : AppCompatImageView(co
         camera.applyToCanvas(canvas)
         camera.restore()
         canvas.clipRect(
-            -picWidthHalf * 2,
+            -imageWidth,
             0f,
-            picWidthHalf * 2,
-            picWidthHalf * 2
+            imageWidth,
+            imageWidth
         )
         canvas.rotate(flipRotation)
         canvas.translate(-picWidthHalf, -picWidthHalf)   //移动到图片到坐标系原点再转动X轴,在Camera中操作是倒叙的
         canvas.drawBitmap(
-            drawable.toBitmap(
-                width,
-                width
-            ), 0f, 0f, paint
+            drawable.toBitmap(imageWidth.toInt(), imageWidth.toInt()), 0f, 0f, paint
         )
         canvas.restore()
 
         //图片上半部分
         canvas.save()
-        canvas.rotate(-flipRotation)
         canvas.translate(picWidthHalf, picWidthHalf)
+        canvas.rotate(-flipRotation)
         camera.save()
         camera.rotateX(topFlip)
         camera.applyToCanvas(canvas)
         camera.restore()
-        canvas.clipRect(
-            -picWidthHalf * 2,
-            -picWidthHalf * 2,
-            picWidthHalf * 2,
-            0f
-        )
+
+        canvas.clipRect(-imageWidth, -imageWidth, imageWidth, 0f)
         canvas.rotate(flipRotation)
         canvas.translate(-picWidthHalf, -picWidthHalf)   //移动到图片到坐标系原点再转动X轴,在Camera中操作是倒叙的
         canvas.drawBitmap(
-            drawable.toBitmap(
-                width,
-                width
-            ), 0f, 0f, paint
+            drawable.toBitmap(imageWidth.toInt(), imageWidth.toInt()), 0f, 0f, paint
         )
         canvas.restore()
 
-        /*
-
-
-             canvas.save();
-        canvas.translate(PADDING + IMAGE_WIDTH / 2, PADDING + IMAGE_WIDTH / 2);
-        canvas.rotate(-flipRotation);
-        camera.save();
-        camera.rotateX(topFlip);
-        camera.applyToCanvas(canvas);
-        camera.restore();
-        canvas.clipRect(- IMAGE_WIDTH, - IMAGE_WIDTH, IMAGE_WIDTH, 0);
-        canvas.rotate(flipRotation);
-        canvas.translate(- (PADDING + IMAGE_WIDTH / 2), - (PADDING + IMAGE_WIDTH / 2));
-        canvas.drawBitmap(Utils.getAvatar(getResources(), (int) IMAGE_WIDTH), PADDING, PADDING, paint);
-        canvas.restore();
-        *   绘制下半部分
-        canvas.save();
-        canvas.translate(PADDING + IMAGE_WIDTH / 2, PADDING + IMAGE_WIDTH / 2);
-        canvas.rotate(-flipRotation);
-        camera.save();
-        camera.rotateX(bottomFlip);
-        camera.applyToCanvas(canvas);
-        camera.restore();
-        canvas.clipRect(- IMAGE_WIDTH, 0, IMAGE_WIDTH, IMAGE_WIDTH);
-        canvas.rotate(flipRotation);
-        canvas.translate(- (PADDING + IMAGE_WIDTH / 2), - (PADDING + IMAGE_WIDTH / 2));
-        canvas.drawBitmap(Utils.getAvatar(getResources(), (int) IMAGE_WIDTH), PADDING, PADDING, paint);
-        canvas.restore();
-        * */
-
     }
 
-    fun rotateX3d(angle: Int) {
-        bottomFlip = angle.toFloat()
-        invalidate()
-    }
 }
