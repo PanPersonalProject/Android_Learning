4c4110fde3b0700d8b81d342e365a2152f90accf
pan <panqiwork@gmail.com>
2024-10-19 12:55:36 +0800

    Compose学习： LayoutModifier 和 Modifier.layout()

--- app/src/main/java/pan/lib/baseandroidframework/ui/main/compose_demo/ComposeModifierDemo.kt
+++ app/src/main/java/pan/lib/baseandroidframework/ui/main/compose_demo/ComposeModifierDemo.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -109,4 +111,71 @@ fun Modifier.customPaddingModifier(): Modifier = composed {
     Modifier
         .padding(padding)
         .clickable { padding += 8.dp }
+}
+
+/*Modifier.layout() 的用处是修改对应组件的尺寸与位置偏移
+它适用于给组件增加装饰效果。 所谓「装饰效果」就是不干涉组件内部测量布局，只在外部调整，例如 padding。
+Compose 每一个组件最终都会生成 LayoutNode 对象，作为这个组件的代表存在内存中。
+它们的测量与布局由 LayoutNode.remeasure() 和 LayoutNode.replace() 方法负责，
+其实前者已经做完了所有工作，包括布局的计算，后者只是用计算的结果布局一下。*/
+@Preview
+@Composable
+fun LayoutModifierDemo() {
+    Box(
+        Modifier
+            .background(Color.Green)
+    ) {  // 背景为绿色的容器
+        Text(
+            "Hello, World!",
+            Modifier
+                .layout { measurable, constraints ->
+                    // 将 padding 设置为 10dp，并转换为像素单位
+                    var padding = 10.dp.roundToPx()
+
+                    // 测量文本组件的实际大小，同时对其进行约束调整，左右、上下留出 padding 空间
+                    val placeable = measurable.measure(
+                        constraints.copy(
+                            maxWidth = constraints.maxWidth - padding * 2,  // 水平方向左右各留出 padding
+                            maxHeight = constraints.maxHeight - padding * 2  // 垂直方向上下各留出 padding
+                        )
+                    )
+
+                    // 通过 layout 函数设置组件最终的宽度和高度
+                    layout(placeable.width + padding * 2, placeable.height + padding * 2) {
+                        // 将测量的元素放置在指定位置，这里是 (padding, padding) 的位置
+                        placeable.place(padding, padding)
+                    }
+                }
+                .background(Color.Blue)  // 在 layout 调整后的文本上添加蓝色背景
+        )
+    }
+}
+
+/*
+跟踪LayoutNodeLayoutDelegate.performMeasure()会发现：
+    Modifier后添加的子LayoutNode会先被调用，然后再调用之前的父LayoutNode。
+
+例如：
+Box(Modifier.padding(10.dp).padding(20.dp)) //最终的padding是30dp
+生成的 outerWrapper 是这样的：
+ModifiedLayoutNode(
+  modifier = PaddingModifier(10.dp),
+  wrapped = ModifiedLayoutWrapper(
+    modifier = PaddingModifier(20.dp),
+    wrapped = InnerPlaceable
+  )
+)
+*/
+
+@Preview
+@Composable
+fun LayoutModifierDemo2() {
+    //最终结果是50dp，父LayoutNode要求子LayoutNode的上限是50dp，子LayoutNode虽然写了100dp，但是会服从父LayoutNode
+    Box(
+        Modifier
+            .size(50.dp)
+            .size(100.dp)
+            .background(Color.Green)
+    )
+
 }
