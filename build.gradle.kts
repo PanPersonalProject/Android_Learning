e4f4e233f9fb0b0a197e5ea5398a95817d6a189e
pan <panqiwork@gmail.com>
2024-12-29 22:21:28 +0800

    del build src,加快构建速度

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id(libs.plugins.androidApplication.get().pluginId) apply false
-    id(libs.plugins.kotlinAndroid.get().pluginId) apply false
-    id(libs.plugins.androidLibrary.get().pluginId) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinKsp) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
